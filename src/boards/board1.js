c804d1a0cb8e1b1ea836c778c5bad41600589488
Russell Elliott <russell@quadsquad.net>
2022-09-16 16:58:46 -0700

    no errors, but board still doesn't update

--- src/boards/board1.js
+++ src/boards/board1.js
@@ -3,21 +3,38 @@ import { Chess } from "chess.js"; // import Chess from  "chess.js"(default) if r
 import { Chessboard } from "react-chessboard";
 
 export default function PlayRandomMoveEngine() {
-  const game = new Chess();
-  console.log(game.fen());
+  const [game, setGame] = useState(new Chess());
+
+  function makeAMove(move) {
+    var FEN = game.fen();
+    console.log(FEN)
+    //const gameCopy = { ...game };
+    const gameCopy = new Chess(FEN)
+    const result = gameCopy.move(move);
+    setGame(gameCopy);
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    makeAMove(possibleMoves[randomIndex]);
+  }
 
   function onDrop(sourceSquare, targetSquare) {
-    console.log(sourceSquare);
-    console.log(targetSquare);
-
-    game.move({
+    const move = makeAMove({
       from: sourceSquare,
       to: targetSquare,
-      //promotion: 'q' // always promote to a queen for example simplicity
+      promotion: 'q' // always promote to a queen for example simplicity
     });
 
-    console.log(game.fen());
-    return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
+    // illegal move
+    if (move === null) return false;
+    
+    setTimeout(makeRandomMove, 200);
+    return true;
   }
 
   return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
