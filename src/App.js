3373cd48d23e2a59e7f070cc6e076d0816fd9a83
Russell Elliott <russell@quadsquad.net>
2023-01-23 15:40:55 -0800

    basic board setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Board1 from "./boards/board1";
+import DecayChess from "./boards/DecayChess";
 import Home from "./Home"
 
 //For the router/navbar stuff
@@ -12,11 +13,13 @@ function App() {
         <nav>
           <Link to="/"> Home </Link>
           <Link to="/board1"> Board1</Link>
+          <Link to="/DecayChess"> Decay Chess</Link>
                   
         </nav>
         <Routes>
               <Route path="/" element={<Home/>} />
               <Route path="/board1" element={ <Board1 /> } />
+              <Route path="/DecayChess" element={ <DecayChess /> } />
         </Routes>
       </Router>
    
