e54bc648cf5aaf79aadab680ea4fd0e661840b91
Russell Elliott <russell@quadsquad.net>
2023-01-23 15:42:02 -0800

    updates

--- src/boards/DecayChess.js
+++ src/boards/DecayChess.js
@@ -15,12 +15,12 @@ export default function PlayRandomMoveEngine() {
     return result; // null if the move was illegal, the move object if the move was legal
   }
 
-  function makeRandomMove() {
+  /*function makeRandomMove() {
     const possibleMoves = game.moves();
     if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return; // exit if the game is over
     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
     makeAMove(possibleMoves[randomIndex]);
-  }
+  }*/
 
   function onDrop(sourceSquare, targetSquare) {
     //works for traditional move, but not promotion
